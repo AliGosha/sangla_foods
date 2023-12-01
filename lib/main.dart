3667880c8adfc44063b53c5e9347f3475618cfd3
AliGosha <mianadnanaliali@gmail.com>
2023-12-01 23:40:36 +0500

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:sangla_foods/screens/SignUp.dart';
+import 'package:sangla_foods/screens/login.dart';
 
 import 'firebase_options.dart';
 
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: SignUp(),
+      home: Login_Page(),
     );
   }
 }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,10 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:sangla_foods/screens/Widgets/customButton.dart';
 
 import 'Widgets/my_text_field.dart';
 
-class Login_Page extends StatelessWidget {
+class Login_Page extends StatefulWidget {
   @override
+  State<Login_Page> createState() => _Login_PageState();
+}
+
+class _Login_PageState extends State<Login_Page> {
+  final formkey = GlobalKey<FormState>();
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+  @override
+  Future loginAuth() async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(
+              email: email.text, password: password.text);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("This user is not Registered"),
+          ),
+        );
+      } else if (e.code == 'wrong-password') {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Please Enter Correct Password"),
+          ),
+        );
+      }
+    }
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
@@ -34,33 +65,35 @@ class Login_Page extends StatelessWidget {
             Column(
               children: [
                 MyTextField(
-                  hintText: "UserName",
+                  hintText: "Email",
                   icon: Icons.person_outline,
                   iconColor: Colors.white,
                   obsecureText: false,
-                  controller: null,
+                  controller: email,
                   validator: (value) {
                     if (value!.isEmpty ||
-                        RegExp(r'^[a-z A-Z]+$ ').hasMatch(value!)) {
-                      return 'Please enter Last Name Correctly';
+                        RegExp(r'^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$ ')
+                            .hasMatch(value!)) {
+                      return 'Please enter Correct email';
                     } else
                       return null;
                   },
-                  KeyBoardType: TextInputType.name,
+                  KeyBoardType: TextInputType.emailAddress,
                 ),
                 SizedBox(
                   height: 20,
                 ),
                 MyTextField(
-                  hintText: "PassWord",
+                  hintText: "Password",
                   icon: Icons.lock,
                   iconColor: Colors.white,
                   obsecureText: false,
-                  controller: null,
+                  controller: password,
                   validator: (value) {
                     if (value!.isEmpty ||
-                        RegExp(r'^[a-z A-Z]+$ ').hasMatch(value!)) {
-                      return 'Please enter Last Name Correctly';
+                        RegExp(r'(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$ ')
+                            .hasMatch(value!)) {
+                      return 'Please enter correct password';
                     } else
                       return null;
                   },
